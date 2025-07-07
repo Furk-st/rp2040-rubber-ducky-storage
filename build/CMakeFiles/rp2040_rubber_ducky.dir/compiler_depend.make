2d16e1a5f080fc31b9018f12bee84f5c916727fb
FURK <faizanmazhr3632@gmail.com>
2025-07-07 16:21:18 -0400

    first commit

---
+++ build/CMakeFiles/rp2040_rubber_ducky.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for rp2040_rubber_ducky.
+# This may be replaced when dependencies are built.
---
+++ build/_deps/picotool-build/data_locs.cpp
@@ -0,0 +1,7 @@
+
+#include <vector>
+#include <string>
+
+std::vector<std::string> data_locs = {
+    "./","/home/kali/rp2040_rubber_ducky/build/_deps/picotool/"
+};
